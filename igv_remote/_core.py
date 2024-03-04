a610859ef3d7cf7f92f16c50833b5ea27a24e31a
Claudia Chu <cchu@broadinstitute.org>
2024-03-04 11:16:06 -0500

    have every send command connect and disconnect from IGV server

--- igv_remote/_core.py
+++ igv_remote/_core.py
@@ -10,28 +10,24 @@ import re
 def _append_id(filename, id):
     return "{0}_{2}.{1}".format(*filename.rsplit('.', 1) + [id])
 
-def _parse_loc(chromosome, pos1, pos2=None, expand=20):
-    if expand < 20:
-        print("IGV expands left and right margin by at least 20bp")
-        expand=20
-    start_pos = int(pos1-expand)
-    if pos2 is None:
-        end_pos = int(pos1+expand)
-    else:
-        end_pos = int(pos2)
-    start_pos ='{:,}'.format(int(start_pos))
-    end_pos = '{:,}'.format(int(end_pos))
-    position= '{}:{}-{}'.format(chromosome, start_pos, end_pos)
-    print("Position to view: {}".format(position))
-    return position
-        
+VALID_VIEW_OPTS = ['squish', 'collapse', 'expand']
+
 class IGV_remote:
-    sock=None
-    
-    def __init__(self, view_type='collapsed', sort='base'): 
-        self.sock = None
-        self.set_viewopts(view_type=view_type, sort=sort)
-    
+
+    def __init__(self, host="127.0.0.1", port=60151, view_type='collapse', sort='base', verbose=True, recv_timeout=60):
+        self.set_viewopts(view_type,  sort)
+        self.HOST = host
+        self.PORT = port
+
+        self.verboseprint = print if verbose else lambda *a, **k: None
+        self.recv_timeout = recv_timeout # time to wait for a response from IGV before exiting (seconds)
+
+    def set_viewopts(self, view_type,  sort):
+        if view_type not in VALID_VIEW_OPTS:
+            raise ValueError(f"view_type must be one of {VALID_VIEW_OPTS}. Input was {view_type}")
+        self._view_type = view_type
+        self._sort = sort
+
     def set_saveopts(self, img_dir, img_basename, img_init_id=0) :
         # check if path is absolute and exits
         if not os.path.exists(img_dir):
@@ -47,62 +43,58 @@ class IGV_remote:
         self._img_fulldir = img_fulldir
         self._img_basename = img_basename
         self._img_id = img_init_id
-    
-    def set_viewopts(self, view_type,  sort):
-        if view_type not in ['squished', 'collapsed', 'expanded']:
-            raise ValueError("view_type must be one of [squished, collapsed, expanded]")
-        self._view_type = view_type
-        self._sort = sort
+        
+    def send_cmds(self, cmds):
+        for cmd in cmds:
+            self._send_cmd(cmd)
 
-    def connect(self, host="127.0.0.1", port=60151):
-        assert self.sock is None
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((host, port))
-        print("socket initialized")
-    
+    def _send_cmd(self, cmd, timeout=20):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.HOST, self.PORT))
+        
+        format_cmd = f'{cmd} \n'.encode('utf-8')
+        self.verboseprint(f"sending command: {format_cmd}")
+        self.socket.send(format_cmd)
+
+        self.socket.settimeout(timeout) # if IGV times out, we recommend you restart IGV
+        self.verboseprint(self.socket.recv(4096))
+        
+        self.socket.close()
+
     def new(self):
-        self._send("new ")
+        self._send_cmd('new')
 
-    def _send(self, cmd):
-        assert self.sock is not None
-        s=self.sock
-        cmd = cmd + '\n'
-        s.send(cmd.encode('utf-8'))
-        s.recv(2000).decode('utf-8').rstrip('\n')
-    
     def load(self, *urls):
-        print(urls)
-        # self._send("new ")
+        self.verboseprint(urls)
+
         if len(urls) < 1:
             raise ValueError("Please provide at least one URL to load")
         for url in urls:
-            self._send("load %s" % url)
+            self._send_cmd("load %s" % url)
             self._adjust_viewopts()
 
-
-    def _adjust_viewopts(self):
-        
-        # specify view options
-        if self._view_type == "squished":
-            self._send( "squish ")
-        elif self._view_type == "collapsed":
-            self._send( "collapse ")
-        elif self._view_type == "expanded":
-            self._send("expand ")
+    def _parse_loc(self, chromosome, pos1, pos2=None, expand=20):
+        if expand < 20:
+            print("IGV expands left and right margin by at least 20bp")
+            expand=20
+        start_pos = int(pos1-expand)
+        if pos2 is None:
+            end_pos = int(pos1+expand)
         else:
-            print("view_type other than squished/collapsed/expanded cannot be understood, will use expand")
-            self._send("expand ")
+            end_pos = int(pos2)
+        start_pos ='{:,}'.format(int(start_pos))
+        end_pos = '{:,}'.format(int(end_pos))
+        position= '{}:{}-{}'.format(chromosome, start_pos, end_pos)
+        self.verboseprint("Position to view: {}".format(position))
+        return position
 
-        self._send( "sort {}".format(self._sort))
-    
-    def goto(self, 
-             chromosome, start_pos, end_pos=None, expand=20):
+    def goto(self, chromosome, start_pos, end_pos=None, expand=20):
         """
         if only start_pos is supplied, we will expand view range by 'expand' parameter
         """
 
-        position = _parse_loc(chromosome, start_pos, end_pos, expand)
-        self._send( "goto %s" % position)
+        position = self._parse_loc(chromosome, start_pos, end_pos, expand)
+        self._send_cmd( "goto %s" % position)
         # make sure viewopts are preserved
         self._adjust_viewopts()
     
@@ -125,20 +117,19 @@ class IGV_remote:
         
         positions = []
         for (_, chrv), (_, posv) in zip(chrpos['chr'].items(), chrpos['pos'].items()):
-            positions.append(_parse_loc(chrv, posv, None, expand))
-
-        self._send("goto {}".format(" ".join(positions)))
+            positions.append(self._parse_loc(chrv, posv, None, expand))
+        self._send_cmd("goto {}".format(" ".join(positions)))
         # make sure viewopts are preserved
         self._adjust_viewopts()
 
     def snapshot(self): # snapshot as-is
         assert self._img_fulldir is not None, "Please set view optins with ir.set_saveopts() first"
-        self._send( "snapshotDirectory %s" % self._img_fulldir)
+        self._send_cmd( "snapshotDirectory %s" % self._img_fulldir)
         newname = _append_id(self._img_basename, self._img_id)
-        self._send( "snapshot %s" % newname)
+        self._send_cmd( "snapshot %s" % newname)
         self._img_id += 1
 
-    def close(self):
-        self.sock.close()
-        print("socket closed")
-        self.sock = None
+    def _adjust_viewopts(self):
+        self._send_cmd(f"{self._view_type}")
+        self._send_cmd(f"sort {self._sort}")
+
